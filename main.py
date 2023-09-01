11ffd903e32e83ae66dd898354ed1191c465cab7
Задача 1 <prohorsn@gmail.com>
2023-09-01 21:49:30 +0700

    Задача 10

--- main.py
+++ main.py
@@ -4,19 +4,16 @@
 # 5 -> 1 0 1 1 0
 # 2
 
-N = int(input('Введите количество монет '))
-orel = reshka = 0
-for i in range(N):
-    x = int(input('Орел(1) или решка(0)? '))
-    if x == 1:
-        orel += 1
+n = int(input())
+count_zero = 0
+count_one = 0
+for i in range(n):
+    x = int(input())
+    if x == 0:
+        count_zero += 1
     else:
-        reshka += 1
-if orel < reshka:
-    print(f'Переверните {orel} монет с орла на решку, их меньше всего')
-elif orel == reshka:
-    print(f'Количество орлов и решек одинаково, по {orel} штук')
+        count_one += 1
+if count_one > count_zero:
+    print(count_zero)
 else:
-    print((f'Переверните {reshka} монет с решки на орла, их меньше всего'))
-
-
+    print(count_one)
